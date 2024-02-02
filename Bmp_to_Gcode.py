cd15bd2420750f399adfce0b491c46dc543f75c7
Ericw <eweissm1@asu.edu>
2024-02-01 20:32:40 -0700

    New COmmit

--- Bmp_to_Gcode.py
+++ Bmp_to_Gcode.py
@@ -12,23 +12,26 @@ import potrace
 def Img_to_Gcode(OriginalImageAddress):
 
     img = Image.open(OriginalImageAddress)
+    img_bitmap = img.tobitmap()
+    print(img_bitmap)
+    ary = np.array(img_bitmap)
 
-    ary = np.array(img)
+    # # Split the three channels
+    # r, g, b = np.split(ary, 3, axis=2)
+    # r = r.reshape(-1)
+    # g = r.reshape(-1)
+    # b = r.reshape(-1)
+    #
+    # # Standard RGB to grayscale
+    # bitmap = list(map(lambda x: 0.299 * x[0] + 0.587 * x[1] + 0.114 * x[2],
+    #                   zip(r, g, b)))
+    # print(bitmap)
+    #
+    # bitmap = np.array(bitmap).reshape([ary.shape[0], ary.shape[1]])
+    # bitmap = np.dot((bitmap > 128).astype(float), 255)
+    # im = Image.fromarray(bitmap.astype(np.uint8))
 
-    # Split the three channels
-    r, g, b = np.split(ary, 3, axis=2)
-    r = r.reshape(-1)
-    g = r.reshape(-1)
-    b = r.reshape(-1)
-
-    # Standard RGB to grayscale
-    bitmap = list(map(lambda x: 0.299 * x[0] + 0.587 * x[1] + 0.114 * x[2],
-                      zip(r, g, b)))
-    bitmap = np.array(bitmap).reshape([ary.shape[0], ary.shape[1]])
-    bitmap = np.dot((bitmap > 128).astype(float), 255)
-    im = Image.fromarray(bitmap.astype(np.uint8))
-
-    bmp = potrace.Bitmap(im)  # convert image array  into Pypotrace bitmap object
+    bmp = potrace.Bitmap(ary)  # convert image array  into Pypotrace bitmap object
 
     path = bmp.trace()
 
