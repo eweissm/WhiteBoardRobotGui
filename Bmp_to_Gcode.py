a85dfb62ca7b21e9eac54896f77809ae3face3f0
Ericw <eweissm1@asu.edu>
2024-02-02 20:14:32 -0700

    New COmmit

--- Bmp_to_Gcode.py
+++ Bmp_to_Gcode.py
@@ -35,38 +35,45 @@ def Img_to_Gcode(OriginalImageAddress):
             end_point_x, end_point_y = segment.end_point
             if segment.is_corner:
                 c_x, c_y = segment.c
-                xvals.append(c_x)
-                yvals.append(c_y)
+                xvals.append(float(c_x))
+                yvals.append(float(c_y))
             else:
                 c1_x, c1_y = segment.c1
                 c2_x, c2_y = segment.c2
 
-                temp_x_list, temp_y_list = bezier_to_points(xvals[-1], yvals[-1], c1_x, c1_y,c2_x, c2_y, end_point_x, end_point_y)
-                xvals.append(temp_x_list)
-                yvals.append(temp_y_list)
+                temp_x_list, temp_y_list = bezier_to_points(curve.start_point, segment.c1, segment.c2, segment.end_point)
+                xvals= xvals +temp_x_list
+                yvals = yvals + temp_y_list
 
-   # print(xvals)
+    print(xvals)
 
     # plot
     fig, ax = plt.subplots()
     ax.plot(xvals, yvals)
     plt.show()
 
-def bezier_to_points(xvals, yvals, c1_x, c1_y,c2_x, c2_y, end_point_x, end_point_y):
+# Type alias for a point
+point = tuple[float, float]
+def bezier_to_points(p1: point, p2: point, p3: point, p4: point):
     numSegments = 10
+
+    x_list =[]
+    y_list = []
     for t in np.linspace(0, 1, num=numSegments):
-        x = (
-            p1[0] * math.pow(1 - t, 3)
-            + 3 * p2[0] * math.pow(1 - t, 2) * t
-            + 3 * p3[0] * (1 - t) * math.pow(t, 2)
-            + p4[0] * math.pow(t, 3)
-        )
-        y = (
-            p1[1] * math.pow(1 - t, 3)
-            + 3 * p2[1] * math.pow(1 - t, 2) * t
-            + 3 * p3[1] * (1 - t) * math.pow(t, 2)
-            + p4[1] * math.pow(t, 3)
-        )
-        yield (x, y)
+        x_list.append(float(
+                p1[0] * math.pow(1 - t, 3)
+                + 3 * p2[0] * math.pow(1 - t, 2) * t
+                + 3 * p3[0] * (1 - t) * math.pow(t, 2)
+                + p4[0] * math.pow(t, 3)
+        ))
+        y_list.append(float(
+                p1[1] * math.pow(1 - t, 3)
+                + 3 * p2[1] * math.pow(1 - t, 2) * t
+                + 3 * p3[1] * (1 - t) * math.pow(t, 2)
+                + p4[1] * math.pow(t, 3)
+        ))
+    print(x_list)
+
+    return x_list, y_list
 
 Img_to_Gcode("Example Image.bmp")
